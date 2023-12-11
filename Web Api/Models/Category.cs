cddd1b9883f21eb00824ba111d3e29668cb7f0d1
ErzhanKub <137794574+ErzhanKub@users.noreply.github.com>
2023-12-11 17:14:08 +0600

    Created models

---
+++ Web Api/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web_Api.Models;
+
+public sealed record Category
+{
+    public long Id { get; init; }
+
+    [Required(ErrorMessage = "Name is required")]
+    [StringLength(100, ErrorMessage = "Name cannot be longer than 100 characters")]
+    public required string Name { get; init; }
+    public List<Product> Products { get; set; } = new List<Product>();
+    public Dictionary<string, string> AdditionalFields { get; init; } = new Dictionary<string, string>();
+}
